e1c0a11f4d37e10e915cca5a8d3b10e60f4e64ae
NotSoGreeeen <jandrico7658@student.pps.net>
2024-05-14 10:03:20 -0700

    More falling sands
    almost works
    Is diagonally instantiated
    can't detect collisions

--- FirstTest.py
+++ FirstTest.py
@@ -13,49 +13,56 @@ squares = []
 
 grid_width = screen.get_width() // 50
 grid_height = screen.get_height() // 50
-grid = [[False for _ in range(grid_width)] for _ in range(grid_height)]
+grid = [[0 for _ in range(grid_height)] for _ in range(grid_width)]
+print(grid[24])
 
 
-def Square(left, top, width, height):
-    grid_x = left // width
-    grid_y = top // height
+def MoveSquare(x, y):
+    try:
+        if grid[x][y] == 1:
+            print("exists")
+            if grid[x][y - 1] == 0:
+                print("moved")
+                grid[x][y] = 0
+                grid[x][y + 1] = 1
+                return (x, y + 1)
+            else:
+                print("Something's below me")
+                return (x, y)
+        else:
+            return(x, y)
+    except:
+        print("Broken :(")
+        return(x, y)
 
-    if grid_y < len(grid) and grid_x < len(grid[0]) and grid[grid_y][grid_x]:
-        return None
-
+def DrawSquare(left, top, width, height):
     pygame.draw.rect(screen, "red", (left, top, width, height), 2)
-
-    if grid_y < len(grid) and grid_x < len(grid[0]):
-        grid[grid_y][grid_x] = True
-
-    new_top = top + height
-    if new_top < screen_height:
-        return (left, new_top)
-    else:
-        return None
 
 
 while running:
-
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    squares.append((mouse_x, mouse_y))
-
+            if event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                squares.append(((mouse_x // 50) - 1, (mouse_y // 50) - 1))
+                grid[(mouse_x // 50) - 1][(mouse_y // 50) -1] = 1
     screen.fill("black")
 
-    updated_squares = []
-    for (x, y) in squares:
-        updated_position = Square(x, y, 5, 5)
-        if updated_position is not None:
-            updated_squares.append(updated_position)
+    #updated_squares = []
+    #for (x, y) in squares:
+    #    updated_position = Square(x, y, 5, 5)
+    #    if updated_position is not None:
+    #        updated_squares.append(updated_position)
 
-    squares = updated_squares
+    #squares = updated_squares
+    
+    for i, (x, y) in enumerate(squares):
+        squares[i] = MoveSquare(x, y)
+        DrawSquare(x * 50, y * 50, 50, 50)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
